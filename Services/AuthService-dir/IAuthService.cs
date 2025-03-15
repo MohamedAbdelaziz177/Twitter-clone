88609f3852fd2de1424c19e30832dcd2864365a4
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2025-03-15 16:36:47 +0200

    Adding 1st Migration - IdentityTables

--- Services/AuthService-dir/IAuthService.cs
+++ Services/AuthService-dir/IAuthService.cs
@@ -1,7 +1,21 @@
-﻿namespace Twitter.Services.AuthService_dir
+﻿using Twitter.DTOs;
+
+namespace Twitter.Services.AuthService_dir
 {
     public interface IAuthService
     {
+        Task<AuthResponseDto> RegisterAsync(RegisterDto registerDto);
 
+        Task<AuthResponseDto> LoginAsync(LoginDto loginDto);
+
+        Task<TokenResponseDto> RefreshTokenAsync(string refreshToken, int userId);
+
+        Task<bool> ConfirmEmailAsync(string email, string code);
+
+        Task<bool> SendConfirmationCode(string email);
+
+        Task<bool> ForgetPasswordAsync(string email);
+
+        Task<bool> ResetPasswordAsync(ResetPasswordDto resetPasswordDto);
     }
 }
