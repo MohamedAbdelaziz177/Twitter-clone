946239c2607c5723528511b886e2cc19e610e500
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2025-03-15 17:30:43 +0200

    Mail Service completely done

---
+++ Services/MailService-dir/IMailService.cs
@@ -0,0 +1,7 @@
+﻿namespace Twitter.Services.MailService_dir
+{
+    public interface IMailService
+    {
+        Task<bool> SendMailAsync(string To, string Subject, string Body);
+    }
+}
---
+++ Services/MailService-dir/MailService.cs
@@ -0,0 +1,56 @@
+﻿
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+using MimeKit.Text;
+
+namespace Twitter.Services.MailService_dir
+{
+    public class MailService : IMailService
+    {
+        private readonly IConfiguration configuration;
+
+        public MailService(IConfiguration configuration) {
+
+            this.configuration = configuration;
+        }
+        public async Task<bool> SendMailAsync(string To, string Subject, string Body)
+        {
+
+            var from = configuration["SmtpSettings:Username"];
+
+            string host = configuration["SmtpSettings:Host"];
+
+            int port = int.Parse(configuration["SmtpSettings:port"]);
+
+            string userName = configuration["SmtpSettings:Username"];
+
+            string password = configuration["SmtpSettings:Password"];
+
+            var msg = new MimeMessage();
+            msg.To.Add(new MailboxAddress(To,To));
+            msg.From.Add(new MailboxAddress(from,from));
+            msg.Subject = Subject;
+            msg.Body = new TextPart(TextFormat.Html) { Text = Body };
+
+
+            using (var smtpClient = new SmtpClient())
+            {
+                try
+                {
+                    await smtpClient.ConnectAsync(host, port, SecureSocketOptions.StartTls);
+                    await smtpClient.AuthenticateAsync(userName, password);
+                    await smtpClient.SendAsync(msg);
+                    await smtpClient.DisconnectAsync(true);
+
+                    return true;
+                }
+                catch (Exception ex) {
+
+                    Console.WriteLine(ex.Message);
+                    return false;
+                }
+            }
+        }
+    }
+}
