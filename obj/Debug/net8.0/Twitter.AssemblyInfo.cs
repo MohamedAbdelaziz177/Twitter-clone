15660e50132c76cf9c5456d5589b0e703311af7d
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2025-03-15 15:03:06 +0200

    Configuring Identity

--- obj/Debug/net8.0/Twitter.AssemblyInfo.cs
+++ obj/Debug/net8.0/Twitter.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Twitter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc054ddd623aeee0da705ef3b756e3e6750041d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+037ae2f4dad84db23a876ee0cd71e5f6ea5cad76")]
 [assembly: System.Reflection.AssemblyProductAttribute("Twitter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Twitter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
