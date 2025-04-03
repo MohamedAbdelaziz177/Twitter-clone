ebf66942fca2a486973ec701d10faad48c7dc754
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2025-04-03 05:22:06 +0200

    Adding Img Service

---
+++ Services/ImgService-dir/IImgService.cs
@@ -0,0 +1,7 @@
+﻿namespace Twitter.Services.ImgService_dir
+{
+    public interface IImgService
+    {
+        Task<string> SaveImageAndGetUrl(IFormFile img);
+    }
+}
---
+++ Services/ImgService-dir/ImgService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Hosting;
+
+namespace Twitter.Services.ImgService_dir
+{
+    public class ImgService : IImgService
+    {
+        private readonly IWebHostEnvironment webHostEnvironment;
+
+        public ImgService(IWebHostEnvironment webHostEnvironment)
+        {
+            this.webHostEnvironment = webHostEnvironment;
+        }
+        public async Task<string> SaveImageAndGetUrl(IFormFile img)
+        {
+            string imgName = Guid.NewGuid().ToString() + Path.GetExtension(img.FileName);
+
+            string folder = Path.Combine(webHostEnvironment.WebRootPath, "Images", "posts");
+
+            string fullImgPath = Path.Combine(folder, imgName);
+
+            using (var fileStream = new FileStream(fullImgPath, FileMode.Create))
+            {
+                await img.CopyToAsync(fileStream);
+            }
+
+            return fullImgPath;
+
+        }
+    }
+}
