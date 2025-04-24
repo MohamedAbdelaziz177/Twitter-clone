698c088e10f28cae4130fcfd5539f10e159c4ce3
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2025-04-24 17:40:43 +0200

    Adding profile service

--- Services/ProfileService-dir/IProfileService.cs
+++ Services/ProfileService-dir/IProfileService.cs
@@ -1,6 +1,12 @@
-﻿namespace Twitter.Services.ProfileService_dir
+﻿using Twitter.DTOs.ProfileDtos;
+
+namespace Twitter.Services.ProfileService_dir
 {
     public interface IProfileService
     {
+        Task<ProfileDto> GetById(int id);
+        Task<ProfileDto> GetByUserId(string userId);
+
+        Task<ProfileDto> UpdateProfile(int id, UpdateProfileDto profileDto);
     }
 }
--- Services/ProfileService-dir/ProfileService.cs
+++ Services/ProfileService-dir/ProfileService.cs
@@ -1,6 +1,55 @@
-﻿namespace Twitter.Services.ProfileService_dir
+﻿using Twitter.DTOs.ProfileDtos;
+using Twitter.Exceptions;
+using Twitter.Mappers;
+using Twitter.Model;
+using Twitter.Services.ImgService_dir;
+using Twitter.Unit_of_work;
+
+namespace Twitter.Services.ProfileService_dir
 {
-    public class ProfileService
+    public class ProfileService : IProfileService
     {
+        private readonly IUnitOfWork unitOfWork;
+        private readonly IImgService imgService;
+
+        public ProfileService(IUnitOfWork unitOfWork, IImgService imgService) 
+        {
+            this.unitOfWork = unitOfWork;
+            this.imgService = imgService;
+        }
+
+        public async Task<ProfileDto> GetById(int id)
+        {
+            var profile =  await unitOfWork.profileRepo.GetByIdAsync(id);
+
+            if (profile == null) throw new NotFoundException("Profile not found");
+
+            return profile.toDto();
+        }
+
+        public async Task<ProfileDto> GetByUserId(string userId)
+        {
+            var profile = await unitOfWork.profileRepo.GetByUserIdAsync(userId);
+
+            if (profile == null) throw new NotFoundException("Profile not found");
+
+            return profile.toDto();
+        }
+
+
+        public async Task<ProfileDto> UpdateProfile(int id, UpdateProfileDto profileDto)
+        {
+            var profile = await unitOfWork.profileRepo.GetByIdAsync(id);
+
+            if (profile == null) throw new NotFoundException("Profile not found");
+
+            profile = profile.fromDto(profileDto);
+            profile.PhotoUrl = await imgService.SaveImageAndGetUrl(profileDto.Img);
+
+            await unitOfWork.profileRepo.UpdateAsync(profile); 
+
+            return profile.toDto();
+
+        }
     }
 }
