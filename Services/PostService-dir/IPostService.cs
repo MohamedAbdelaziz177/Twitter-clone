901c11ed15c82d533a128cc20606ea1746048827
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2025-04-24 13:56:45 +0200

    nearly finished post service

--- Services/PostService-dir/IPostService.cs
+++ Services/PostService-dir/IPostService.cs
@@ -5,6 +5,12 @@ namespace Twitter.Services.PostService_dir
 {
     public interface IPostService
     {
-        
+        Task<PostDto> GetById(int id);
+
+        Task<PostDto> AddNewPost(CreateUpdatePostDto postDto, string userId);
+
+        Task<PostDto> UpdatePost(int id, CreateUpdatePostDto postDto, string userId);
+
+        Task Delete(int id, string userId);
     }
 }
--- Services/PostService-dir/PostService.cs
+++ Services/PostService-dir/PostService.cs
@@ -1,41 +1,82 @@
 ﻿using Twitter.DTOs.PostDtos;
+using Twitter.Exceptions;
+using Twitter.Mappers;
+using Twitter.Model;
+using Twitter.Services.ImgService_dir;
 using Twitter.Unit_of_work;
+using UnauthorizedAccessException = System.UnauthorizedAccessException;
 
 namespace Twitter.Services.PostService_dir
 {
-    public class PostService
+    public class PostService : IPostService
     {
         private readonly IUnitOfWork unitOfWork;
+        private readonly IImgService imgService;
 
-        public PostService(IUnitOfWork unitOfWork)
+        public PostService(IUnitOfWork unitOfWork, IImgService imgService)
         {
             this.unitOfWork = unitOfWork;
+            this.imgService = imgService;
         }
 
-        public PostDto GetById(int id)
+        public async Task<PostDto> GetById(int id)
         {
-            return null;
-        }
+            var post = await unitOfWork.PostRepo.GetPostWithCommentsAndUser(id);
 
+            if (post == null) throw new NotFoundException("No such a post");
+
+            return post.toDto();
+        }
+  
         public List<PostDto> GetTimeline(string userId) 
         {
             return null; 
         }
 
-        public PostDto AddNewPost(CreateUpdatePostDto postDto)
+        public async Task<PostDto> AddNewPost(CreateUpdatePostDto postDto, string userId)
         {
-            return null;
+            Post post = new Post();
+            post.UserId = userId;
+
+            post = post.fromDto(postDto);
+            
+            if(postDto.Img != null)
+            post.ImgUrl = await imgService.SaveImageAndGetUrl(postDto.Img);
+
+
+            try { await unitOfWork.PostRepo.InsertAsync(post); }
+            catch (Exception ex) { throw new IOException("Post Not saved - try again"); }
+
+            return post.toDto();
         }
 
-        public PostDto UpdatePost(CreateUpdatePostDto postDto) 
+        public async Task<PostDto> UpdatePost(int id, CreateUpdatePostDto postDto, string userId) 
         {
-            return null;
+            
+            var post = await unitOfWork.PostRepo.GetByIdAsync(id);
+
+            if (post == null) throw new NotFoundException("No such Post");
+
+            if (post.UserId != userId) throw new
+                    UnauthorizedAccessException("You are not authorized to acess this post");
+
+
+            post = post.fromDto(postDto);
+
+            if(post.ImgUrl != null)
+            post.ImgUrl = await imgService.SaveImageAndGetUrl(postDto.Img);
+
+            try { await unitOfWork.PostRepo.UpdateAsync(post); }
+            catch (Exception e) { throw new IOException("Post Not saved - try again"); }
+
+            return await GetById(id);
         }
 
-        public void Delete(int id, string userId) 
+        public async Task Delete(int id, string userId)
         {
+            try { await unitOfWork.PostRepo.DeleteAsync(id); }
+            catch (Exception ex) { throw new IOException("Post Not deleted - try again"); }
         }
-
 
     }
 }
