d803af72547a0824dc0ceeaed2db8c072c45e5c7
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2025-04-24 12:51:06 +0200

    Adding Profile Details Dto

---
+++ DTOs/ProfileDtos/ProfileDto.cs
@@ -0,0 +1,15 @@
+﻿using Twitter.DTOs.PostDtos;
+
+namespace Twitter.DTOs.ProfileDtos
+{
+    public class ProfileDto
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public string Bio { get; set; } = string.Empty;
+
+        public string ImgUrl { get; set; } = string.Empty;
+
+        public List<PostDto> PostDtos { get; set; } = new();
+    }
+}
