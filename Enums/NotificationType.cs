77b4c78d650fb8a3521bf02d3f8ed3777c8c049c
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2025-05-01 23:09:11 +0300

    Adding notification feature entity

---
+++ Enums/NotificationType.cs
@@ -0,0 +1,12 @@
+﻿namespace Twitter.Enums
+{
+    public enum NotificationType
+    {
+        POST = 1,
+        COMMENT,
+        LIKE,
+        FOLLOW,
+        GUIDELINES_VIOLATION,
+        SECURITY_THREAT
+    }
+}
---
+++ Model/Notification.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Twitter.Enums;
+
+namespace Twitter.Model
+{
+    public class Notification
+    {
+        public int Id { get; set; }
+
+        public NotificationType Type { get; set; }
+
+        public string Description { get; set; } = string.Empty;
+
+        [ForeignKey(nameof(Post))]
+        public int? PostId { get; set; }
+
+        public Post? Post { get; set; }
+
+        [ForeignKey(nameof(Comment))]
+        public int? CommentId { get; set; }
+
+        public Comment? Comment { get; set; }
+
+
+    }
+}
