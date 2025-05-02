7bd2177a97a08a6fa6ebe30ca30c82b4b0c30eb9
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2025-05-02 23:06:19 +0300

    customizing bookmarks repository

--- Repository/BookmarkRepo/BookmarkRepo.cs
+++ Repository/BookmarkRepo/BookmarkRepo.cs
@@ -1,4 +1,6 @@
-﻿using Twitter.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using Twitter.Controllers;
+using Twitter.Data;
 using Twitter.Model;
 using Twitter.Repository.GenericRepo;
 
@@ -8,6 +10,21 @@ namespace Twitter.Repository.BookmarkRepo
     {
         public BookmarkRepo(AppDbContext con) : base(con)
         {
+        }
+
+        public async Task<List<Bookmark>?> GetMyBookmarks(string userId)
+        {
+            return await dbSet.Where(b => b.UserId == userId)
+                .Include(b => b.User)
+                .ThenInclude(u => u.profile)
+                .Include(b => b.Post)
+                .ThenInclude(p => p.comments) 
+                .ToListAsync();
+        }
+
+        public async Task<Bookmark?> GetbookmarkByUserAndPostId(string userId, int postId)
+        {
+            return await dbSet.FirstOrDefaultAsync(b => b.UserId == userId && b.PostId == postId);
         }
     }
 }
--- Repository/BookmarkRepo/IBookmarkRepo.cs
+++ Repository/BookmarkRepo/IBookmarkRepo.cs
@@ -5,5 +5,7 @@ namespace Twitter.Repository.BookmarkRepo
 {
     public interface IBookmarkRepo : IGenericRepo<Bookmark>
     {
+        Task<List<Bookmark>?> GetMyBookmarks(string userId);
+        Task<Bookmark?> GetbookmarkByUserAndPostId(string userId, int postId);
     }
 }
