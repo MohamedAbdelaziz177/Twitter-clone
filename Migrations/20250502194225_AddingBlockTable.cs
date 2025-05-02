1a687144be25035c15f25012d398734d8ba951ac
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2025-05-02 23:10:29 +0300

    Migrating block entity to DB

---
+++ Migrations/20250502194225_AddingBlockTable.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Twitter.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingBlockTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "BookmarkedAt",
+                table: "Bookmarks",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "blocks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BlockerId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    BlockedId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_blocks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_blocks_AspNetUsers_BlockedId",
+                        column: x => x.BlockedId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_blocks_AspNetUsers_BlockerId",
+                        column: x => x.BlockerId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_blocks_BlockedId",
+                table: "blocks",
+                column: "BlockedId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_blocks_BlockerId",
+                table: "blocks",
+                column: "BlockerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "blocks");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "BookmarkedAt",
+                table: "Bookmarks",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+    }
+}
