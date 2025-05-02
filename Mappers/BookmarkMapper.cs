067a2f8e5825fed13d9559da698e34d4479df884
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2025-05-02 23:09:25 +0300

    Bookmark and like list mappers customized

---
+++ Mappers/BookmarkMapper.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Hosting;
+using Twitter.DTOs.BookmarkDtos;
+using Twitter.Model;
+
+namespace Twitter.Mappers
+{
+    public static class BookmarkMapper
+    {
+        public static BookmarkDto toDto(this Bookmark bookmark)
+        {
+            BookmarkDto dto = new();
+
+            dto.Id = bookmark.Id;
+            dto.AuthorId = bookmark.User.Id;
+            dto.Author = bookmark.User.FirstName + " " + bookmark.User.LastName;
+            dto.CreatedAt = bookmark.BookmarkedAt;
+            dto.ImgUrl = bookmark.Post.ImgUrl;
+            dto.CommentsCount = bookmark.Post.BookmarksCount;
+            dto.LikesCount = bookmark.Post.LikesCount;
+            dto.BookmarksCount = bookmark.Post.BookmarksCount;
+
+            foreach (Comment comment in bookmark.Post.comments)
+                dto.Comments.Add(comment.toDto());
+
+            return dto;
+        }
+    }
+}
---
+++ Mappers/LikeMapper.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.CompilerServices;
+using Twitter.DTOs.AuthDtos;
+using Twitter.DTOs.LikeDtos;
+using Twitter.Model;
+
+namespace Twitter.Mappers
+{
+    public static class LikeMapper
+    {
+        public static LikeDto toDto(this Like like)
+        {
+
+            LikeDto dto = new();
+
+            dto.UserId = like.UserId;
+            dto.UserName = like.User.FirstName + " " + like.User.LastName;
+            dto.ProfileImgUrl = like.User.profile.PhotoUrl;
+            dto.ProfileId = like.User.profile.Id;
+
+            return dto;
+
+        }
+    }
+}
