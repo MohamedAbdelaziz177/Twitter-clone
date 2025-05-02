bab93b2a255079ae29ef550ca4415111255dec3c
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2025-05-02 23:07:17 +0300

    Adding & customizing like repository

--- Repository/LikeRepo/ILikeRepo.cs
+++ Repository/LikeRepo/ILikeRepo.cs
@@ -5,5 +5,10 @@ namespace Twitter.Repository.LikeRepo
 {
     public interface ILikeRepo : IGenericRepo<Like>
     {
+        Task<bool> IsLikedByUser(string userId, int PostId);
+
+        Task<Like?> GetByUserAndPost(string userId, int PostId);
+
+        Task<List<Like>?> GetByPostId(int postId);
     }
 }
--- Repository/LikeRepo/LikeRepo.cs
+++ Repository/LikeRepo/LikeRepo.cs
@@ -1,4 +1,5 @@
-﻿using Twitter.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using Twitter.Data;
 using Twitter.Model;
 using Twitter.Repository.GenericRepo;
 
@@ -8,6 +9,27 @@ namespace Twitter.Repository.LikeRepo
     {
         public LikeRepo(AppDbContext con) : base(con)
         {
+        }
+
+        public async Task<bool> IsLikedByUser(string userId, int PostId)
+        {
+            var like =
+            await dbSet.FirstOrDefaultAsync(l => l.UserId == userId && l.PostId == PostId);
+
+            return like != null;
+        }
+
+        public async Task<Like?> GetByUserAndPost(string userId, int PostId)
+        {
+            return await dbSet.FirstOrDefaultAsync(l => l.UserId == userId && l.PostId == PostId);
+        }
+
+        public async Task<List<Like>?> GetByPostId(int postId)
+        {
+            return await dbSet.Where(l => l.PostId == postId)
+                .Include(l => l.User)
+                .ThenInclude(u => u.profile).ToListAsync();
+
         }
     }
 }
