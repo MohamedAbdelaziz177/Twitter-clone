3c543982d76ad49e30158b3f9b0df3a301445626
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2025-05-03 19:52:39 +0300

    Completing all actions inside block controller

--- Controllers/BlockController.cs
+++ Controllers/BlockController.cs
@@ -1,11 +1,37 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Twitter.Services.BlockService_dir;
 
 namespace Twitter.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class BlockController : ControllerBase
+    [Authorize]
+    public class BlockController : BasePlusUserController
     {
+        private readonly IBlockService blockService;
+
+        public BlockController(IBlockService blockService)
+        {
+            this.blockService = blockService;
+        }
+
+        [HttpPost("block")]
+        public async Task<IActionResult> BlockUser([FromQuery] string blockedId)
+        {
+            await blockService.BlockUser(userId, blockedId);
+
+            return NoContent();
+        }
+
+        [HttpDelete("unblock")]
+        public async Task<IActionResult> UnblockUser([FromQuery] string blockedId)
+        {
+            await blockService.UnblockUser(userId, blockedId);
+
+            return NoContent();
+        }
+
     }
 }
